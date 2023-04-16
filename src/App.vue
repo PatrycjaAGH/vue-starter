54738060733b4bf672e1614e4151193a7156f9e5
pszczepa <pszczepa@d17-430-pc-01.ipa.ii.agh.edu.pl>
2023-04-16 14:05:21 +0200

    zachownaie komponentu

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="my-app">
-    <h1>Hello MWO!</h1>
+  <h1>Twój e-mail to {{ email }}</h1>
+  <input type="email" v-model="email">
+
+  <div v-if="email.length < 10">Ale masz krótki adres!</div>
+  <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+  <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+
+  <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+
+    {{ email.length }}
+
+
   </div>
 </template>
 
 <script>
+
 export default {
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    }
+  },
+data() {
+  return {
+    email: 'patrycja.barbara.szczepanska@gmail.com',
+    password: ''
+  };
+}
 
 }
 </script>
